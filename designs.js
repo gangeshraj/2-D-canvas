1e060effa3b81470073b3e4b32559305ec5fbdea
gangesh <gangeshraj1@gmail.com>
2017-09-07 12:35:29 +0530

    yes it is the first project

---
+++ designs.js
@@ -0,0 +1,36 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+function makeGrid() {
+    //Your code goes here!
+    $('#sizePicker').on("submit", function(evt) {
+           let ch="";
+                evt.preventDefault();
+           const heightof=document.getElementById('input_height').value;
+           const widthof=document.getElementById('input_width').value;
+               for(let i=0;i<heightof;i++)
+               {
+                    ch+="<tr>"
+                    for(let j=0;j<widthof;j++)
+                    {
+                            ch+="<td class='data'></td>"
+                    }
+                    ch+="</tr>"
+               }
+           //alert(ch);
+               $("tr").html("");
+               $("table").prepend(ch);
+           });
+        const colorof=document.getElementById('colorPicker').value;
+        //alert(colorof);
+            $("#colorPicker").on("input", function(){
+                    alert("colorchanging");
+                        $("td").on("click",function(){
+                            alert(colorof);
+                            console.log(this);
+                            $(this).css("background-color",colorof);
+                        });
+               });
+    }
+makeGrid();
