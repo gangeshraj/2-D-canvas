98c3ed662e41d09746ad8b2e0b08c963ef1027dd
gangeshraj <30901825+gangeshraj@users.noreply.github.com>
2018-05-06 14:33:04 +0530

    now it is more visual

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ A wonderfl grid drawing project
 
 #   HOSTED AT
 #   https://gangeshraj.github.io/2-D-canvas/
+
+
+here you can draw painting and fill the canvas according to your own color when you load the site than its already painted make it meaningful by your creativity
--- designs.js
+++ designs.js
@@ -1,3 +1,4 @@
+
 function makeGrid() {
     $('#sizePicker').on("submit", function(evt) {
            let ch="";
@@ -22,3 +23,27 @@ function makeGrid() {
            });
     }
 makeGrid();
+
+function populate(){
+    document.getElementById('input_height').value=35;
+    document.getElementById('input_width').value=35;
+    const heightof=document.getElementById('input_height').value;
+    const widthof=document.getElementById('input_width').value;
+    let ch="";
+    for(let i=0;i<heightof;i++)
+       {
+            ch+="<tr>"
+            for(let j=0;j<widthof;j++)
+            {
+                    ch+="<td></td>"
+            }
+            ch+="</tr>"
+       }
+       $("tr").html("");
+       $("table").prepend(ch);
+       $( "td" ).each(function( index ) {
+            //console.log(index);
+            let coloring=Math.floor(Math.random()*222222);
+            $(this).css("background-color",'#'+coloring);
+       });
+}
